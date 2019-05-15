970fe1cbe70fbd6334c9dd75ec7e2566ba5a84bd
Jahaziel <jahaziel.vaz@gmail.com>
2019-05-14 21:21:19 -0600

    New tutorial setup complete

---
+++ src/ReduxDemo.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+//Redux
+import { createStore } from 'redux';
+
+class ReduxDemo extends Component {
+  render(){
+    //Step 1- Store: Reducer, State (In other words the 2 parameters that the createStore function takes the first one is the reducer, and the second one is the default state)
+
+
+
+    return(
+      <div>
+        <h1>Test</h1>
+      </div>
+    )
+  }
+}
+
+export default ReduxDemo;
